b91ad495b7c66e4f8e33f178d5ea70e6612b96c2
Giovanni Antonaccio <giovanniantonaccio@gmail.com>
2019-07-21 19:36:49 -0400

    Add props
    Removing the static information and replacing by props. The values in state are fixed for now. Lately they will come from an API.

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,17 +1,121 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Feed.css";
 
 import Post from "./Post.js";
 
-function Header() {
-  return (
-    <div className="feed-body">
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-    </div>
-  );
+class Feed extends Component {
+  state = {
+    posts: [
+      {
+        id: "1",
+        author: {
+          name: "Júlio Ancantara",
+          avatar: "https://i.pravatar.cc/150?u=a042581f4e29046704d"
+        },
+        date: "21 Jul 2019",
+        content:
+          "Galera, alguém mais fazendo o Bootcamp GoStack da Rocketseat?",
+        comments: [
+          {
+            id: 1,
+            author: {
+              name: "Cézar Toledo",
+              avatar: "https://i.pravatar.cc/150?u=a042581f4e79046704d"
+            },
+            date: "21 Jul 2019",
+            content:
+              "Também estou fazendo o Bootcamp e estou adorando! Estou no terceiro módulo sobre Node e já tenho minha API dos desafios contruída!"
+          },
+          {
+            id: 2,
+            author: {
+              name: "Clara Avelar",
+              avatar: "https://i.pravatar.cc/150?u=a048583f9e79046784d"
+            },
+            date: "21 Jul 2019",
+            content:
+              "Que maaaaassa! Estou pensando em me inscrever na próxima turma pra ver qual é desse Bootcamp GoStack, dizem que os devs saem de lá com super poderes!"
+          }
+        ]
+      },
+      {
+        id: "2",
+        author: {
+          name: "Júlio Ancantara",
+          avatar: "https://i.pravatar.cc/150?u=a042581f4e29046704d"
+        },
+        date: "21 Jul 2019",
+        content:
+          "Galera, alguém mais fazendo o Bootcamp GoStack da Rocketseat?",
+        comments: [
+          {
+            id: 1,
+            author: {
+              name: "Cézar Toledo",
+              avatar: "https://i.pravatar.cc/150?u=a042581f4e79046704d"
+            },
+            date: "21 Jul 2019",
+            content:
+              "Também estou fazendo o Bootcamp e estou adorando! Estou no terceiro módulo sobre Node e já tenho minha API dos desafios contruída!"
+          },
+          {
+            id: 2,
+            author: {
+              name: "Clara Avelar",
+              avatar: "https://i.pravatar.cc/150?u=a048583f9e79046784d"
+            },
+            date: "21 Jul 2019",
+            content:
+              "Que maaaaassa! Estou pensando em me inscrever na próxima turma pra ver qual é desse Bootcamp GoStack, dizem que os devs saem de lá com super poderes!"
+          }
+        ]
+      },
+      {
+        id: "3",
+        author: {
+          name: "Júlio Ancantara",
+          avatar: "https://i.pravatar.cc/150?u=a042581f4e29046704d"
+        },
+        date: "21 Jul 2019",
+        content:
+          "Galera, alguém mais fazendo o Bootcamp GoStack da Rocketseat?",
+        comments: [
+          {
+            id: 1,
+            author: {
+              name: "Cézar Toledo",
+              avatar: "https://i.pravatar.cc/150?u=a042581f4e79046704d"
+            },
+            date: "21 Jul 2019",
+            content:
+              "Também estou fazendo o Bootcamp e estou adorando! Estou no terceiro módulo sobre Node e já tenho minha API dos desafios contruída!"
+          },
+          {
+            id: 2,
+            author: {
+              name: "Clara Avelar",
+              avatar: "https://i.pravatar.cc/150?u=a048583f9e79046784d"
+            },
+            date: "21 Jul 2019",
+            content:
+              "Que maaaaassa! Estou pensando em me inscrever na próxima turma pra ver qual é desse Bootcamp GoStack, dizem que os devs saem de lá com super poderes!"
+          }
+        ]
+      }
+    ]
+  };
+
+  render() {
+    const { posts } = this.state;
+
+    return (
+      <div className="feed-body">
+        {posts.map(post => (
+          <Post key={post.id} {...post} />
+        ))}
+      </div>
+    );
+  }
 }
 
-export default Header;
+export default Feed;
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,38 +1,29 @@
 import React from "react";
 import "./Post.css";
+import { comment } from "postcss";
 
-import user1 from "../images/user1.png";
-import user2 from "../images/user2.png";
-import user3 from "../images/user3.png";
-
-function Post() {
+function Post({ author, date, content, comments }) {
   return (
     <div className="post-content">
       <div className="post-header">
-        <img src={user1} alt="User 1" />
-        <div className="post-info">
-          <strong>Júlio Ancantara</strong>
-          <small>21 Jul 2019</small>
+        <img src={author.avatar} alt="Avatar" />
+        <div className="post-header-info">
+          <strong>{author.name}</strong>
+          <small>{date}</small>
         </div>
       </div>
-      <p>Galera, alguém mais fazendo o Bootcamp GoStack da Rocketseat?</p>
+
+      <p>{content}</p>
+
       <ul className="comment">
-        <li>
-          <img src={user2} alt="User 2" />
-          <p>
-            <strong>Cézar Toledo </strong> Também estou fazendo o Bootcamp e
-            estou adorando! Estou no terceiro módulo sobre Node e já tenho minha
-            API dos desafios contruída!
-          </p>
-        </li>
-        <li>
-          <img src={user3} alt="User 2" />
-          <p>
-            <strong>Gabriel Lisboa </strong> Que maaaaassa! Estou pensando em me
-            inscrever na próxima turma pra ver qual é desse Bootcamp GoStack,
-            dizem que os devs saem de lá com super poderes!
-          </p>
-        </li>
+        {comments.map(comment => (
+          <li key={comment.id}>
+            <img src={comment.author.avatar} alt="Avatar" />
+            <p>
+              <strong>{comment.author.name}</strong> {comment.content}
+            </p>
+          </li>
+        ))}
       </ul>
     </div>
   );
